7da3aeaec7f717690d2e027b01633dff21e71c34
Fon To <fonto_85@hotmail.com>
2019-10-09 18:46:02 +0200

    vad hander

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let button = document.getElementById("button");
+let board = document.querySelectorAll(".board");
 
 button.addEventListener('click',create)
 
@@ -8,5 +9,30 @@ function create(){
     div.append(input);
     div.setAttribute('draggable', true);
     div.setAttribute('class',"job");
+    div.setAttribute('id',"abc")
     document.body.append(div)
+    div.addEventListener('dragstart', function(evt){
+        evt.dataTransfer.setData("text", evt.target.id);
+        console.log(evt)
+        console.log("dragging.....");
+    })
+}
+
+
+for(const emp of board){
+    emp.addEventListener("dragover", dragOver)
+    emp.addEventListener("drop", dragDrop)
+}
+
+function dragOver(e){
+    e.preventDefault();
+    console.log("over")
+}
+
+function dragDrop(evt){
+    evt.preventDefault();
+    let data = evt.dataTransfer.getData("text");
+    console.log(evt.dataTransfer.getData("text"))
+    console.log("dropped")
+    evt.target.appendChild(document.getElementById(data));
 }
